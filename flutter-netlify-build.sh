40c80b94618cb58f493aeb3493e7be60811e2f1a
Radek Svarz <radek.svarz@gmail.com>
2020-05-18 18:06:19 +0200

    netlify script update

--- flutter-netlify-build.sh
+++ flutter-netlify-build.sh
@@ -1,13 +1,28 @@
-#!/bin/sh
+#!/bin/bash
 FLUTTER_BRANCH=`grep channel: .metadata | sed 's/  channel: //g'`
 FLUTTER_REVISION=`grep revision: .metadata | sed 's/  revision: //g'`
 
-git clone https://github.com/flutter/flutter.git
+# Setup flutter
+FLUTTER=`which flutter`
+if [ $? -eq 0 ]
+then
+  # Flutter is installed
+  FLUTTER=`which flutter`
+else
+  # Get flutter
+  git clone https://github.com/flutter/flutter.git
+  FLUTTER=flutter/bin/flutter
+fi
+
 cd flutter
 git checkout $FLUTTER_BRANCH
 git pull origin $FLUTTER_BRANCH
 git checkout $FLUTTER_REVISION
 cd ..
 
-flutter/bin/flutter config --enable-web
-flutter/bin/flutter build web
+# Setup dart
+DART=`echo $FLUTTER | sed 's/flutter$/cache\/dart-sdk\/bin\/dart/'`
+echo $DART
+
+$FLUTTER config --enable-web
+$FLUTTER build web --release
