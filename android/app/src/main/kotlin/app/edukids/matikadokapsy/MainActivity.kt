153a48bffc35f81ede4af72ba9c835c15cfd5329
Radek Svarz <radek.svarz@gmail.com>
2020-07-22 15:20:38 +0200

    flutter v2 embedding

---
+++ android/app/src/main/kotlin/app/edukids/matikadokapsy/MainActivity.kt
@@ -0,0 +1,18 @@
+package cz.edukids.matikadokapsy
+
+//import android.os.Bundle // v1 flutter embedding
+//import io.flutter.app.FlutterActivity // v1 flutter embedding
+//import io.flutter.plugins.GeneratedPluginRegistrant // v1 flutter embedding
+import io.flutter.embedding.android.FlutterActivity
+
+
+class MainActivity: FlutterActivity() {
+
+// v1 flutter embedding
+//  override fun onCreate(savedInstanceState: Bundle?) {
+//    super.onCreate(savedInstanceState)
+//    GeneratedPluginRegistrant.registerWith(this)
+//  }
+
+
+}
